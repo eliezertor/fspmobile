a2c9204fbc7e66afb0396570dc42176f7a8a38e0
Eliezer Toribio <eliezertor@gmail.com>
2021-03-12 14:37:47 -0500

    trying apisauce

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MoviesApi from './app/api/movies';
 
 export default function App() {
-  MoviesApi.search(movies);
+  const [movies, setMovies] = useState();
+  const [result, setResult] = useState();
+
+  const search = async (value) => {
+    const result = await MoviesApi.searchName(value);
+    if (!result.ok) return setResult(result.data);
+  };
+
+  const searchType = async (movie) => {
+    const result = await MoviesApi.search(movie);
+    if (!result.ok) return setMovies(result);
+  };
+
+
+  MoviesApi.apiMdb.get()
+
+  console.log(movies);
+  console.log(result);
+
+  searchType('movie');
+  search('jack reacher');
 
   return (
     <View style={styles.container}>
--- app/api/movies.js
+++ app/api/movies.js
@@ -1,18 +1,24 @@
 import { create } from 'apisauce';
-// import { TMDB_API_KEY } from '@env';
-// import 'dotenv/config';
 import Constants from 'expo-constants';
 
 // TODO: add variable after /3/ to define what type of object we will retrieve {movie/tv/search/list} etc.
 
+let searchValue;
+let movieValue;
+
 const search = (typeOfSearch) => {
-  const searchValue = typeOfSearch;
-//   console.log(Constants.manifest.extra.TMDB_API_KEY, searchValue);
+  searchValue = typeOfSearch;
+  console.log(searchValue, movieValue);
   return searchValue;
 };
 
-// const apiMdb = create({
-//   baseURL: `https://api.themoviedb.org/3/search/${searchValue}?api_key=${TMDB_API_KEY}`,
-// });
+const searchName = (value) => {
+  movieValue = value;
+  return movieValue;
+};
 
-export default { search, create };
+const apiMdb = create({
+  baseURL: `https://api.themoviedb.org/3/search/${searchValue}?api_key=${Constants.manifest.extra.TMDB_API_KEY}&language=en-US&query=${movieValue}`,
+});
+
+export default { search, create, searchName, apiMdb };
